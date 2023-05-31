2ca150131fe2d155a0e1298451c113aacca06222
Jess Aebi <jesseyaeee@gmail.com>
2023-05-31 16:52:16 +0200

    stopwatch works, but is a bit slower than an actual stopwatch. Must be fixed.

--- projects/stopwatch/stopwatch.js
+++ projects/stopwatch/stopwatch.js
@@ -4,41 +4,18 @@ let timer = JSON.parse(localStorage.getItem('timer')) || {
   milliSeconds: 0
 }
 
-let timerIsRunning = 0;
-
-if (timerIsRunning){ // If site is refreshed while timer was running, timer should continue running
-  runTimer();
-}
-
 let startButton = document.querySelector('.js-start-button');
 let stopButton = document.querySelector('.js-stop-button');
 let resetButton = document.querySelector('.js-reset-button');
 console.log(resetButton);
 
+let additionalMinutesDigit = handleSingleDigits('.additional-minutes-digit', timer.minutes, 9);
 document.querySelector('.js-stopwatch-minutes').innerHTML = timer.minutes;
 
-//let additionalSecondsDigit = handleSingleDigits('.additional-seconds-digit', timer.seconds, 9);
-
-let additionalSecondsDigit = document.querySelector('.additional-seconds-digit');
-if (timer.seconds > 9){ // When site is refreshed, activate '0' digit, so we have two digits all the time
-  additionalSecondsDigit.innerHTML = '';
-}
-
-function handleSingleDigits(HTMLClass, toCheck, number) {
-  let additionalDigit = document.querySelector(HTMLClass);
-  let returnValue;
-  if (toCheck > number) {
-    returnValue = additionalDigit.innerHTML = '';
-  } 
-  return returnValue;
-}
-
+let additionalSecondsDigit = handleSingleDigits('.additional-seconds-digit', timer.seconds, 9);
 document.querySelector('.js-stopwatch-seconds').innerHTML = timer.seconds;
 
-let additionalMillisecondsDigit = document.querySelector('.additional-milliSeconds-digit');
-if (timer.milliSeconds > 9){
-  additionalMillisecondsDigit.innerHTML = '';
-}
+let additionalMillisecondsDigit = handleSingleDigits('.additional-milliSeconds-digit', timer.milliSeconds, 9);
 document.querySelector('.js-stopwatch-milliSeconds').innerHTML = timer.milliSeconds;
 
 let intervalId;
@@ -81,7 +58,6 @@ function runTimer(){
     }
 
     localStorage.setItem('timer', JSON.stringify(timer));
-    timerIsRunning = true;
   }, 10)
 }
 
@@ -108,5 +84,11 @@ function renderTime() {
   document.querySelector('.js-stopwatch-milliSeconds').innerHTML = timer.milliSeconds;
 }
 
-
+function handleSingleDigits(HTMLClass, toCheck, number) {
+  let additionalDigit = document.querySelector(HTMLClass);
+  if (toCheck > number) {
+    additionalDigit.innerHTML = '';
+  }
+  return additionalDigit;
+}
 
